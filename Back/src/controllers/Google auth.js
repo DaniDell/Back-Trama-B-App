e88740c974574d6b9c31c5d0bb03a79879753c16
GGuenov <german.guenov@gmail.com>
2024-01-12 20:17:41 +0000

    .

---
+++ Back/src/controllers/Google auth.js
@@ -0,0 +1,7 @@
+{
+    "sub": "auth0|65a17c2d440be1715ed7ccd6",
+    "nickname": "tramabtextil",
+    "name": "tramabtextil@gmail.com",
+    "picture": "https://s.gravatar.com/avatar/84371f816393537c6b80db75dbdaf3f2?s=480&r=pg&d=https%3A%2F%2Fcdn.auth0.com%2Favatars%2Ftr.png",
+    "updated_at": "2024-01-12T17:52:22.912Z"
+  }
