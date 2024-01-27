3d6d132ece7b9e2abc0c3fde15cb893476155e6b
GGuenov <german.guenov@gmail.com>
2024-01-27 18:29:52 +0000

    .

--- Back/src/server.js
+++ Back/src/server.js
@@ -1,7 +1,20 @@
 const express = require("express");
 const router = require("./routers/index");
+const { auth } = require("express-oauth2-jwt-bearer");
 const app = express();
+
+const jwtCheck = auth({
+  audience: "tramabtextil@gmail.com",
+  issuerBaseURL: "https://dev-qdibpp58r7xpjz3z.uk.auth0.com/",
+  tokenSigningAlg: "RS256",
+});
+// enforce on all endpoints
+app.use(jwtCheck);
 app.use(express.json());
+app.get("/authorized", function (req, res) {
+  res.send("Secured Resource");
+});
 
 app.use(router);
+
 module.exports = app;
