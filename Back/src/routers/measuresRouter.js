6eed56643faa54955341767b974ba455dbe79be8
Daniela Dell Acqua <ddacqua@gmail.com>
2024-02-13 09:45:24 -0300

    Debug autorization midleware token

--- Back/src/routers/measuresRouter.js
+++ Back/src/routers/measuresRouter.js
@@ -5,11 +5,12 @@ const {
   getMeasureHandler,
   getAllByXHandler,
 } = require("../handlers/measuresHandlers");
+const authenticateUser = require('../middlewares/authMiddleware'); // Importar authenticateUser se usa en todas las rutas
 const measuresRouter = Router();
 
-measuresRouter.post("/create", measureCreatorHandler);
-measuresRouter.put("/edit", measureEditorHandler);
-measuresRouter.get("/getby", getAllByXHandler);
-measuresRouter.get("/:id", getMeasureHandler);
+measuresRouter.post("/create", authenticateUser, measureCreatorHandler);
+measuresRouter.put("/edit", authenticateUser, measureEditorHandler);
+measuresRouter.get("/getby",authenticateUser,  getAllByXHandler);
+measuresRouter.get("/:id", authenticateUser, getMeasureHandler);
 
 module.exports = measuresRouter;
--- Back/src/routers/usersRouter.js
+++ Back/src/routers/usersRouter.js
@@ -12,8 +12,8 @@ const usersRouter = Router();
 
 usersRouter.post("/create", userCreatorHandler);
 usersRouter.put("/edit", authenticateUser, editUserHandler); // Usar authenticateUser en la ruta de edición
-usersRouter.get("/:id", authenticateUser, getUserHandler); // Usar authenticateUser en la ruta de obtener usuario por ID
-usersRouter.get("/", authenticateUser, getAllUsersHandler); // Usar authenticateUser en la ruta de obtener todos los usuarios
+usersRouter.get("/:id", getUserHandler); 
+usersRouter.get("/", getAllUsersHandler); 
 usersRouter.delete("/delete/:id", authenticateUser, deleteUserByIdHandler); // Usar authenticateUser en la ruta de eliminación
 usersRouter.post("/login", loginUserHandler); // Agrega la ruta de inicio de sesión
 
