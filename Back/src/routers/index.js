51bc91951d5d5a7788a6711754e1ec1e671d01e1
Daniela Dell Acqua <ddacqua@gmail.com>
2024-03-18 15:11:44 -0300

    ok

--- Back/src/routers/index.js
+++ Back/src/routers/index.js
@@ -7,4 +7,8 @@ const router = Router();
 router.use("/users", usersRouter);
 router.use("/measures", measuresRouter);
 
+router.get("/", (req, res) => {
+    res.send("Backend is working con MongoDb!");
+  });
+
 module.exports = router;
